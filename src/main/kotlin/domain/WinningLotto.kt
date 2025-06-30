91b968f2e759aa8b260dd6afbf44580993f916f1
kanghuijin <rkd8527@naver.com>
2025-06-30 21:40:11 +0900

    Revert "initial commit"
    This reverts commit 2b5c99caa0bc1a4ef2f38ff7acc043c63dbf3581.

--- src/main/kotlin/domain/WinningLotto.kt
+++
@@ -1,9 +0,0 @@
-package org.hj.domain
-
-data class WinningLotto(val numbers: Set<Int>, val bonus: Int) {
-    init {
-        require(numbers.size == 6) { "당첨 번호는 6개여야 합니다." }
-        require(bonus !in numbers) { "보너스 번호는 당첨 번호와 중복될 수 없습니다." }
-        require((numbers + bonus).all { it in 1..45 })
-    }
-}
